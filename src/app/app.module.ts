e97c6dac95210410e8ba5978c09eb6c7d5affe9c
Aryeman <aryeman@gmail.com>
2020-08-19 18:27:40 -0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TeacherComponent } from './teacher/teacher.component';
+import { StudentComponent } from './student/student.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeacherComponent,
+    StudentComponent
   ],
   imports: [
     BrowserModule
